3d5429a507fdd697e401b4b7292a6d23baff5d26
Bonnie Zhou <bonnie_z_123@hotmail.com>
2015-09-19 12:39:10 -0400

    add tableview and connect to code

--- Instabite/ViewController.swift
+++ Instabite/ViewController.swift
@@ -8,7 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    @IBOutlet weak var appTableView: UITableView!
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "MyTestCell")
+        
+        cell.textLabel?.text = "Row #\(indexPath.row)"
+        cell.detailTextLabel?.text = "Subtitle #\(indexPath.row)"
+        
+        return cell
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +34,4 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
-
