9e3b837839c4aa6d63178d9a0d2ca7e905a13c8c
Joy <joy.gao54@gmail.com>
2015-09-19 14:11:44 -0400

    added background img

---
+++ Instabite/SearchViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SearchViewController.swift
+//  Instabite
+//
+//  Created by Joy Gao on 2015-09-19.
+//  Copyright (c) 2015 Bonnie Zhou. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController  {
+    
+}
